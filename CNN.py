069c96ea81b1936df982add13fcf2572006fa956
LAM NGUYEN KHOA <khoalamvn@gmail.com>
2021-05-02 14:43:06 +0700

    Added more images and allow continuously input

--- CNN.py
+++ CNN.py
@@ -1,3 +1,4 @@
+import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
@@ -9,6 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import cv2
+import tensorflow.python.util.deprecation as deprecation
 
 
 def load_data():
@@ -18,8 +20,8 @@ def load_data():
     im_width = train_x.shape[1]
     im_height = train_x.shape[2]
     # Reshape train_x, test_x so it have 1 channel color
-    train_x = train_x.reshape((train_x.shape[0], 28, 28, 1))
-    test_x = test_x.reshape((test_x.shape[0], 28, 28, 1))
+    train_x = train_x.reshape((train_x.shape[0], im_width, im_height, 1))
+    test_x = test_x.reshape((test_x.shape[0], im_width, im_height, 1))
     # one hot encode target values
     train_y = to_categorical(train_y)
     test_y = to_categorical(test_y)
@@ -62,16 +64,17 @@ def train_and_evaluate(data_x, data_y, n_folds=5):
         train_x, train_y, test_x, test_y = data_x[train_index], data_y[train_index], data_x[test_index], data_y[
             test_index]
         # fit model
-        model.fit(train_x, train_y, epochs=1, batch_size=32, validation_data=(test_x, test_y), verbose=0)
+        model.fit(train_x, train_y, epochs=10, batch_size=32, validation_data=(test_x, test_y), verbose=0)
         # evaluate model
         _, acc = model.evaluate(test_x, test_y, verbose=0)
         print('> %.3f' % (acc * 100.0))
         if acc > bestScore:
-            bestModel = model
+            best_model = model
             bestScore = acc
         # stores scores
-    bestModel.save('savedModel')
-    return bestModel
+    best_model.save('savedModel')
+    return best_model
+
 
 def actual_test(model, test_x, test_y):
     _, acc = model.evaluate(test_x, test_y)
@@ -80,17 +83,21 @@ def actual_test(model, test_x, test_y):
 
 def test_new_image(filename, model):
     # Read an image as grayscale
-    print("OHOHOHO")
+    print("Loaded new image.")
     cv2.namedWindow("output", cv2.WINDOW_NORMAL)
     srcImg = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
     dim = (28, 28)
-    resizedImg = cv2.resize(srcImg, dim)
-    cv2.imshow('output', resizedImg)
+    resized_img = cv2.resize(srcImg, dim)
+    cv2.imshow('output', resized_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    resizedImg = resizedImg.reshape((1, 28, 28, 1))
-    ans = model.predict(resizedImg)
-    print(np.argmax(ans[0]))
+    # prepare data
+    resized_img = resized_img.reshape((1, 28, 28, 1))
+    resized_img = resized_img.astype('float32')
+    resized_img = resized_img / 255.0
+    ans = model.predict(resized_img)
+    print('Predicted:', np.argmax(ans[0]))
+    print('Original ans:', ans[0])
 
 
 def step_by_step_visualize():
@@ -124,25 +131,40 @@ def step_by_step_visualize():
     print("Layer3 dim:", draw_data.shape)
 
 
-def driver():
-    # Training the model
-    train_x, train_y, test_x, test_y = load_data()
-    train_x, test_x = normalize_image(train_x, test_x)
-    model = train_and_evaluate(train_x, train_y)
+def driver(load=False, continuous=False):
+    if not load:
+        # Training the model
+        train_x, train_y, test_x, test_y = load_data()
+        train_x, test_x = normalize_image(train_x, test_x)
+        model = train_and_evaluate(train_x, train_y)
+    else:
+        model = keras.models.load_model('savedModel')
 
     # Actual test the test set
-    #actual_test(model, test_x, test_y)
+    # actual_test(model, test_x, test_y)
 
     # Test with custom image
-    # TODO: Load the image and resize to 28x28
-    test_new_image('number1color.jpg', model)
-    # TODO: Convert image to grayscale
-    # TODO: Run the model on it
+    if not continuous:
+        test_new_image('number9.png', model)
+    else:
+        while True:
+            print("=====================================")
+            print("Type in image location (ex: path/to/location) : ")
+            filename = input()
+            if filename == "":
+                break
+            else:
+                test_new_image(filename, model)
+
+    # FIXME: Make sure the image has black/dark background and white/light number
+    # FIXME: Since the training is handwritten image, work best with handwritten image
+    # TODO: Take into account skewed images
 
 
 # MAIN CODE START HERE
 # make Tensorflow use GPU instead of CPU
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+deprecation._PRINT_DEPRECATION_WARNINGS = False
 
 # driver code control the program
-driver()
+driver(True, True)

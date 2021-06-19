8599159a4faae60476f02522cdc99c4f90b48c40
LAM NGUYEN KHOA <khoalamvn@gmail.com>
2021-06-19 15:09:41 +0700

    Can now separate word vertically

--- handwriting_recognition.py
+++ handwriting_recognition.py
@@ -3,10 +3,10 @@ import numpy as np
 import model_training
 
 
-def display_image_cv2(img):
-    cv2.namedWindow('output', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('output', 200, 200)
-    cv2.imshow('output', img)
+def display_image_cv2(img, window_name="output"):
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(window_name, 200, 200)
+    cv2.imshow(window_name, img)
     cv2.waitKey(0)
 
 
@@ -15,7 +15,7 @@ def ratio_resize(img):
     Resized an image while keep its ratio
     """
     (old_h, old_w) = img.shape[:2]
-    # First, make the image square shape
+    # First, make the image shape become square
     if old_w > old_h:
         diff = old_w - old_h
         if diff % 2 == 0:
@@ -30,11 +30,11 @@ def ratio_resize(img):
         else:
             img = cv2.copyMakeBorder(img, 0, 0, diff//2, diff-(diff//2), cv2.BORDER_CONSTANT, 0)
     # Then resize it normally
-    resized_img = cv2.resize(img, (16, 20), cv2.INTER_AREA)
+    resized_img = cv2.resize(img, (16, 20), cv2.INTER_AREA)  # Chose 16,20 because the horizontal sides are more empty
     return resized_img
 
 
-def sort_contours(cnts, img_dim, method="left-to-right"):
+def sort_contours(cnts, img_dim, method):
     """ Copied from imutils package"""
     # initialize the reverse flag and sort index
     reverse = False
@@ -62,9 +62,11 @@ def sort_contours(cnts, img_dim, method="left-to-right"):
     return cnts, bounding_boxes
 
 
-def preprocess_image(src_img):
-    prep_imgs = list()
-    loc_imgs = list()
+def separate_vertical_lines(src_img, visual=False):
+    """ This function try to find lines of words and sort them"""
+    line_imgs = []
+    loc_lines = []
+    # Turn image into grayscale: black background with white color font
     gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
     avg_color_per_row = np.average(gray_img, axis=0)
     avg_color = np.average(avg_color_per_row, axis=0)
@@ -74,16 +76,38 @@ def preprocess_image(src_img):
     else:
         print("This is a dark image")
         ret, thresh_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
-    blur_img = cv2.blur(thresh_img, (6, 6))  # Strong blur to detect 2-parts letters such as i and j
-    display_image_cv2(blur_img)
+    blur_img = cv2.blur(thresh_img, (64, 2))  # Horizontal blur to detect word lines
+    if visual:
+        display_image_cv2(blur_img, "blur_img in separate_vertical_lines")
     # Detect each digit in the image = bounding box
-    contours, hierarchy = cv2.findContours(blur_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    _, bounding_boxes = sort_contours(contours, blur_img.shape)
+    contours, _ = cv2.findContours(blur_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, bounding_boxes = sort_contours(contours, blur_img.shape, "top-to-bottom")
+    for box in bounding_boxes:
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        loc_lines.append((x, y, w, h))
+        cropped_img = thresh_img[y:y+h, x:x+w]
+        padded_img = cv2.copyMakeBorder(cropped_img, 50, 50, 50, 50, cv2.BORDER_CONSTANT, 0)
+        if visual:
+            display_image_cv2(padded_img, "word lines")
+        line_imgs.append(padded_img)
+    return line_imgs, loc_lines
+
+
+def preprocess_image(src_img, visual=False):
+    """ This function assume the src_img contain 1 word """
+    prep_imgs = []
+    loc_imgs = []
+    blur_img = cv2.blur(src_img, (4, 4))  # Blur to detect 2-parts letters such as i and j
+    if visual:
+        display_image_cv2(blur_img, "blur_img in preprocess_image")
+    # Detect each digit in the image = bounding box
+    contours, _ = cv2.findContours(blur_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, bounding_boxes = sort_contours(contours, blur_img.shape, "left-to-right")
     for box in bounding_boxes:
         x, y, w, h = box[0], box[1], box[2], box[3]
         loc_imgs.append((x, y, w, h))
-        cropped_img = thresh_img[y:y+h, x:x+w]
-        # After crop image to 16x20, padding to 28x28
+        cropped_img = src_img[y:y+h, x:x+w]
+        # After resize image to 16x20, padding to 28x28
         resized_img = ratio_resize(cropped_img)
         padded_img = cv2.copyMakeBorder(resized_img, 4, 4, 6, 6, cv2.BORDER_CONSTANT, 0)
         # More processing
@@ -94,28 +118,36 @@ def preprocess_image(src_img):
 
 
 def main():
-    model, label_names = model_training.get_trained_model("vgg_model")
+    model, label_names = model_training.get_trained_model("vgg_model3")
     src_img = cv2.imread('test_images/hw_image1.png')
-    prep_imgs, loc_imgs = preprocess_image(src_img)
-    n = len(prep_imgs)
-    result_string = ''
-    for i in range(n):
-        # display_image_cv2(prep_imgs[i])
-        x_test = prep_imgs[i].reshape((1, 28, 28, 1))
-        x_test = x_test.astype('float32')
-        x_test = x_test / 255.0
-        pred = model.predict(x_test)[0]
-        tmp = np.argmax(pred)
-        prob = pred[tmp]
-        print("[Id {} Prob. ] {} - {:.2f}%".format(i, label_names[tmp], prob * 100))
-        result_string = result_string + label_names[tmp]
-        # Draw on image
-        x, y, w, h = loc_imgs[i]
-        cv2.rectangle(src_img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
-        cv2.putText(src_img, label_names[tmp], org=(x - 5, y - 5),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 0), thickness=2)
-        cv2.putText(src_img, "id"+str(i), org=(x + 10, y - 5),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255, 0, 0), thickness=1)
+    line_imgs, loc_lines = separate_vertical_lines(src_img)
+    id_counter = 0
+    result_string = ""
+    for i in range(len(line_imgs)):
+        prep_imgs, loc_imgs = preprocess_image(line_imgs[i])
+        for j in range(len(prep_imgs)):
+            # display_image_cv2(prep_imgs[j])
+            x_test = prep_imgs[j].reshape((1, 28, 28, 1))
+            x_test = x_test.astype('float32')
+            x_test = x_test / 255.0
+            pred = model.predict(x_test)[0]
+            tmp = np.argmax(pred)
+            prob = pred[tmp]
+            # Print probability info for each word
+            print("[Id {} Prob. ] {} - {:.2f}%".format(id_counter, label_names[tmp], prob * 100))
+
+            result_string = result_string + label_names[tmp]
+            # Draw on image
+            x = loc_lines[i][0] + loc_imgs[j][0] - 50
+            y = loc_lines[i][1] + loc_imgs[j][1] - 50
+            w, h = loc_imgs[j][2:4]
+            cv2.rectangle(src_img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+            cv2.putText(src_img, label_names[tmp], org=(x - 5, y - 5),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(0, 255, 0), thickness=2)
+            cv2.putText(src_img, "id"+str(id_counter), org=(x + 10, y - 5),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(255, 0, 0), thickness=1)
+            id_counter += 1
+        result_string += " "  # Space to separate word lines
     print(result_string)
     cv2.imshow('output', src_img)
     cv2.waitKey(0)
@@ -126,5 +158,5 @@ def main():
 if __name__ == "__main__":
     main()
 
-# TODO: Order of words
+# TODO: Order of words horizontally
 # TODO: Difference in 1, l, i, j

f24a9df18b9d83d98e7ea7987019caebd71b5e0b
LAM NGUYEN KHOA <khoalamvn@gmail.com>
2021-06-19 15:09:10 +0700

    Added naive Inception model

--- choose_model.py
+++ choose_model.py
@@ -1,25 +1,29 @@
 import keras
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, Dropout, Input, Activation
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, BatchNormalization, Dropout, Input, Activation, concatenate
 from keras.optimizers import SGD
 
 
 class ModelEnum:
     HOMEMADE_MODEL = 0
     VGG_MODEL = 1
-    RESNET_MODEL = 2
+    INCEPTION_MODEL = 2
+    RESNET_MODEL = 3
 
 
 def create_homemade_model(n_label):
+    """ Aka model that not copy from internet"""
     model = keras.Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
     model.add(BatchNormalization())  # keep data in range(0,1)
     model.add(MaxPool2D((2, 2)))  # down-sampling/ reduce size of data
     model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
-    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(BatchNormalization())
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+    model.add(BatchNormalization())
     model.add(MaxPool2D((2, 2), strides=(2, 2)))
-    model.add(Dropout(0.2))  # randomly drop 20% input to prevent over-fitting
     model.add(Flatten())  # flatten into 1d for Dense layers
     model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))  # speedup training and simplify model
+    model.add(Dropout(0.2))  # randomly drop 20% input to prevent over-fitting
     model.add(Dense(n_label, activation='softmax'))  # normalize output into probability distribution
     opti = SGD(lr=0.01, momentum=0.9)
     model.compile(optimizer=opti, loss='categorical_crossentropy', metrics=['accuracy'])
@@ -52,18 +56,36 @@ def create_vgg_model(n_label):
     return model
 
 
+def inception_module(layer_in, f1, f2, f3):
+    conv1 = Conv2D(f1, (1, 1), padding='same', activation='relu')(layer_in)
+    conv3 = Conv2D(f2, (3, 3), padding='same', activation='relu')(layer_in)
+    conv5 = Conv2D(f3, (5, 5), padding='same', activation='relu')(layer_in)
+    pool = MaxPool2D((3, 3), strides=(1,1), padding='same')(layer_in)
+    # concatenate filters, assumes filters/channels last
+    layer_out = concatenate([conv1, conv3, conv5, pool], axis=-1)
+    return layer_out
+
+
+def create_inception_model(n_label):
+    input_layer = Input(shape=(28, 28, 1))
+    inception_layer1 = inception_module(input_layer, 64, 128, 32)
+    flatten_layer = Flatten()(inception_layer1)
+    output_layer = Dense(n_label, activation='softmax')(flatten_layer)
+    model = keras.Model(inputs=input_layer, outputs=output_layer)
+    opti = SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer=opti, loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
 def residual_module(layer_in, n_filters):
     merge_input = layer_in
     # check if the number of filters needs to be increase, assumes channels last format
     if layer_in.shape[-1] != n_filters:
-        merge_input = Conv2D(n_filters, (1,1), padding='same', activation='relu', kernel_initializer='he_normal')(layer_in)
-    # conv1
-    conv1 = Conv2D(n_filters, (3,3), padding='same', activation='relu', kernel_initializer='he_normal')(layer_in)
-    # conv2
-    conv2 = Conv2D(n_filters, (3,3), padding='same', activation='linear', kernel_initializer='he_normal')(conv1)
+        merge_input = Conv2D(n_filters, (1, 1), padding='same', activation='relu', kernel_initializer='he_normal')(layer_in)
+    conv1 = Conv2D(n_filters, (3, 3), padding='same', activation='relu', kernel_initializer='he_normal')(layer_in)
+    conv2 = Conv2D(n_filters, (3, 3), padding='same', activation='linear', kernel_initializer='he_normal')(conv1)
     # add filters, assumes filters/channels last
     layer_out = keras.layers.add([conv2, merge_input])
-    # activation function
     layer_out = Activation('relu')(layer_out)
     return layer_out
 
@@ -82,14 +104,13 @@ def create_resnet_model(n_label):
 def get_model(val, n_label):
     """
     Load a model skeleton to use in training.
-    Not to be confused with get_trained_model(), which is used in handwriting_recognition
-    :param val:
-    :param n_label:
-    :return:
+    Not to be confused with get_trained_model(), which is used in handwriting_recognition.
     """
     if val == ModelEnum.HOMEMADE_MODEL:
         return create_homemade_model(n_label)
     if val == ModelEnum.VGG_MODEL:
         return create_vgg_model(n_label)
+    if val == ModelEnum.INCEPTION_MODEL:
+        return create_inception_model(n_label)
     if val == ModelEnum.RESNET_MODEL:
-        return  create_resnet_model(n_label)
+        return None
